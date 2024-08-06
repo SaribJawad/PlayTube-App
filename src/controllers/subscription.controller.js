bb419c552cb12e8c399f0d6171bb17b29d9fa330
SaribJawad <msssarib12@gmail.com>
2024-08-07 01:10:50 +0500

    Add subscription controllers

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,116 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+  const user = req.user?._id;
+
+  const isSubscribed = await Subscription.findOne({
+    channel: channelId,
+    subscriber: user,
+  });
+
+  if (isSubscribed) {
+    try {
+      const unsubscribe = await Subscription.findByIdAndDelete(
+        isSubscribed?._id
+      );
+
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(200, unsubscribe, "Channel unsubscribed successfully")
+        );
+    } catch {
+      throw new ApiError(500, "Error while unsubscribing");
+    }
+  } else {
+    try {
+      const subscribe = await Subscription.create({
+        channel: channelId,
+        subscriber: user,
+      });
+
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(200, subscribe, "Channel subscribed successfully")
+        );
+    } catch {
+      throw new ApiError(500, "Error while subscribing");
+    }
+  }
+});
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  try {
+    const channelSubscribers = await Subscription.find({
+      channel: channelId,
+      subscriber: { $ne: null },
+    });
+
+    if (channelSubscribers.length === 0) {
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(200, [], "No subscribers found for this channel")
+        );
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          channelSubscribers,
+          "Subscribers fetched successfully"
+        )
+      );
+  } catch {
+    throw new ApiError(500, "Error while fetching subscribers");
+  }
+});
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  const { subscriberId } = req.params;
+
+  try {
+    const subscribedChannels = await Subscription.find({
+      subscriber: subscriberId,
+      channel: { $ne: null },
+    });
+
+    if (subscribedChannels.length === 0) {
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            subscribedChannels,
+            "No subscribed channel found for user"
+          )
+        );
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          subscribedChannels,
+          "Subscribed channel fetched successfully"
+        )
+      );
+  } catch {
+    throw new ApiError(500, "Error while fetching subscribed channel");
+  }
+});
+
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
